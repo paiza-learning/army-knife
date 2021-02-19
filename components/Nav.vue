c6e19cf93e7e0302556c0ca9835a8205e2c8ec9e
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2021-02-19 16:47:50 +0900

    feat: add navbar to default layout

---
+++ components/Nav.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar type="light" variant="light">
+    <b-navbar-brand tag="h1" class="mb-0" to="/">
+      <b-img
+        src="logo.png"
+        class="d-inline-block align-top mx-1"
+        height="30"
+      />army-knife
+    </b-navbar-brand>
+  </b-navbar>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,16 @@
         paiza-learning学生スタッフの作っている便利な業務効率化webアプリ集です。
         <br />誰でも利用することができますが、業務の都合で予告なしに挙動を変更することがあります。
       </b-alert>
+      <div class="my-4">
+        <b-table-simple>
+          <b-tr v-for="tool in tools" :key="tool.id">
+            <b-td>
+              <nuxt-link :to="tool.link">{{ tool.name }}</nuxt-link>
+            </b-td>
+            <b-td>{{ tool.description }}</b-td>
+          </b-tr>
+        </b-table-simple>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +25,20 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  layout: 'no_header',
+  data() {
+    return {
+      tools: [
+        {
+          name: 'set_number',
+          description: 'ソースコードに行番号を付与します。',
+          link: '/set_number',
+        },
+      ],
+    }
+  },
+})
 </script>
 
 <style>
