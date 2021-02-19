c196b458d9e81c1d56c7b2ea10885440a80fd658
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2021-02-19 15:02:54 +0900

    initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-img src="logo.png" size="400" />
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hero">
+    <div>
+      <Logo />
+      <h1 class="display-1 border-bottom border-success">army-knife</h1>
+      <p class="lead">created by paiza-learning</p>
+      <b-alert variant="success" show>
+        paiza-learning学生スタッフの作っている便利な業務効率化webアプリ集です。
+        <br />誰でも利用することができますが、業務の都合で予告なしに挙動を変更することがあります。
+      </b-alert>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+.hero {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
